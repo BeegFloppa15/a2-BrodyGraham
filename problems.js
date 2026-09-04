8dc1376209f16dbfa9224f8ff13b32e6ee953b63
Brody <bjgraham@wpi.edu>
2026-09-04 13:19:50 -0400

    Added a few extra problems

--- problems.js
+++ problems.js
@@ -2,7 +2,13 @@ let problemMap = new Map();
 
 // TODO: Add some more problems
 problemMap.set("2 + 2", [4]);
-problemMap.set("Square Root of 144", [12]);
+problemMap.set("Square root of 144", [12]);
+problemMap.set("6 x 7", [42])
+problemMap.set("6 x 9", [72])
+problemMap.set("15 / 5" [3])
+problemMap.set("5 to the power of 3", [125])
+problemMap.set("Life, the universe, everything", [42])
+
 
 function randomProblem(){
     let index = parseInt(Math.random() * problemMap.size)
